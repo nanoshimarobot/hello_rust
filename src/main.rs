467a83eb538a9ab7f77f41cb276b1cd78c40a5ae
nanoshimarobot <shimada.toyozo.lf@tut.jp>
2023-05-31 23:48:31 +0900

    [update] plot pointcloud around the circumference

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use plotters::prelude::*;
 use rand::{thread_rng, Rng};
+use std::f32::consts::PI;
 
 #[derive(Debug, Clone)]
 struct Vector2 {
@@ -8,17 +9,31 @@ struct Vector2 {
     // s: &'a str,
 }
 
+struct LaserScan {
+    r: f32,
+    theta: f32,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let image_width = 1080;
-    let image_height = 720;
+    let image_height = 1080;
 
     let mut rng = thread_rng();
-    let mut point_cloud: Vec<_> = (0..1000)
+    let mut point_cloud: Vec<_> = (0..500)
         .map(|_| Vector2 {
             x: rng.gen(),
             y: rng.gen(),
         })
         .collect();
+
+    let mut laser_scan: Vec<_> = (0..500)
+        .map(|_| LaserScan {
+            r: 1.0 + rng.gen_range(-0.05f32..0.05f32),
+            theta: rng.gen_range(0f32..PI/2.0),
+        })
+        .collect();
+
+    point_cloud = scan_to_pointcloud(laser_scan);
 
     for i in 0..point_cloud.len() {
         //point_cloud[i] = Vector2 {x: rng.gen(), y: rng.gen()};
@@ -31,15 +46,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let caption = "Point";
     let font = ("monospace", 15);
 
-    let mut chart: ChartContext<BitMapBackend, Cartesian2d<plotters::coord::types::RangedCoordf32, plotters::coord::types::RangedCoordf32>> = ChartBuilder::on(&root)
+    let mut chart: ChartContext<
+        BitMapBackend,
+        Cartesian2d<plotters::coord::types::RangedCoordf32, plotters::coord::types::RangedCoordf32>,
+    > = ChartBuilder::on(&root)
         .caption(caption, font.into_font())
         .margin(10)
         .x_label_area_size(16)
         .y_label_area_size(42)
-        .build_cartesian_2d(0f32..1f32, 0f32..1f32)?;
+        .build_cartesian_2d(-1.1f32..1.1f32, -1.1f32..1.1f32)?;
 
     chart.configure_mesh().draw()?;
-
 
     // let mut point_cloud: Vec<Vector2>;
     // point_cloud.
@@ -60,10 +77,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //         .map(|v| TriangleMarker::new((v.x, v.y), 8, &RED)),
     // )?;
 
-    let point_series = PointSeries::<_,_,Circle<_,_>,_>::new(
-        point_cloud.iter().map(|v|(v.x, v.y)), 5, &RED
-    );
+    let circle = Circle::new((0f32, 0f32), 460, &BLUE);
 
+    let point_series =
+        PointSeries::<_, _, Circle<_, _>, _>::new(point_cloud.iter().map(|v| (v.x, v.y)), 5, &RED);
+
+    chart.plotting_area().draw(&circle)?;
     chart.draw_series(point_series)?;
 
     Ok(())
@@ -77,4 +96,16 @@ fn build_random_vector(mut point_cloud: Vec<Vector2>) -> Vec<Vector2> {
         point_cloud[i].y = rng.gen();
     }
     point_cloud
+}
+
+fn scan_to_pointcloud(mut laser_scan: Vec<LaserScan>) -> Vec<Vector2> {
+    let mut cloud: Vec<Vector2> = Vec::new();
+
+    for i in 0..laser_scan.len() {
+        cloud.push(Vector2 { 
+            x: laser_scan[i].r * f32::cos(laser_scan[i].theta),
+            y: laser_scan[i].r * f32::sin(laser_scan[i].theta),
+        });
+    }
+    cloud
 }
